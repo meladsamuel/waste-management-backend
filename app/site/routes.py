93754ecf9640abf9f8a48486491070d54012a900
meladsamuel <meladsamuel2@gmail.com>
2021-04-03 00:18:09 +0200

    add health check

--- app/site/routes.py
+++ app/site/routes.py
@@ -6,3 +6,8 @@ site = Blueprint('site', __name__)
 @site.route('/')
 def index():
     return "<h1>Welcome to Our Waste Management System</h1>"
+
+
+@site.route('/health')
+def health_check():
+    return "ok"
